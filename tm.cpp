6a38361a86bc7a0449f3ba64b46c04123441d5d7
Ankur Jyoti Phukan <leocreed1994@gmail.com>
2017-03-10 14:18:38 +0530

    Update tm.cpp
    some c++ style

--- tm.cpp
+++ tm.cpp
@@ -1,77 +1,84 @@
 #include<iostream> 
-#include<string.h>
+#include<string>
 #define max_node 5
 #define right 1
 #define left -1
 using namespace std;
-int dir=right; // +1 for right and -1 for left
+// +1 for right and -1 for left
+int dir=right; 
 int mxsize;
-struct link
-{char input;
- char change;
- int dir;
- class node *next; // if you are going to use the class which is nt declared yet then .... you have to add class keyword
- link *neigh_link;// for the other links connected to node
+
+struct link{
+	char input;
+	char change;
+ 	int dir;
+	// if you are going to use the class which is nt declared yet then .... you have to add class keyword
+ 	class node *next;
+	// for the other links connected to node
+ 	link *neigh_link;
 };
 
-class node
-{public :
-   int node_no;
-   int link_no;
-   link *links;//for link linked list
-  node()
-    {node(-1);
+class node{
+	public :
+  	int node_no;
+   	int link_no;
+   	link *links;//for link linked list
+		//Default constructor is calling the parameterized constructor
+  	node(){
+			node(-1);
+     	}
+  	node(int n){
+			node_no=n;
+     	link_no=0;
+     	links=NULL;
      }
-  node(int n)
-    {node_no=n;
-     link_no=0;
-     links=NULL;
-     }
- void linker(char in,char ch,int dir,node *next)//links are establish
-  { link *tmp=new link;
-    tmp->input=in;
-    tmp->change=ch;
-    tmp->dir=dir;
-    tmp->next=next;
-    tmp->neigh_link=links;
-    links=tmp;
-    link_no++;
-   }
- link* check(char in)
-  {link *tmp;
+		//links are establish
+		void linker(char in,char ch,int dir,node *next){
+			link *tmp=new link;
+    	tmp->input=in;
+    	tmp->change=ch;
+    	tmp->dir=dir;
+    	tmp->next=next;
+    	tmp->neigh_link=links;
+    	links=tmp;
+    	link_no++;
+   	}
+ 	link* check(char in){
+		link *tmp;
     tmp=links;
-    while(tmp!=NULL)
-    {if(tmp->input==in)
+    while(tmp!=NULL){
+			if(tmp->input==in)
         break;
-       tmp=tmp->neigh_link;
-      }
+      tmp=tmp->neigh_link;
+   	}
     return tmp;
+  }
+// trans is to move from one to node
+} no[max_node],*trans;
+
+node* node_add(int n){
+	if(n>mxsize)
+  	return NULL;
+	
+  return &no[n];
    }
 
-
-} no[max_node],*trans;// trans is to move from one to node
-
-node* node_add(int n)
-  {if(n>mxsize)
-   return NULL;
-
-   return &no[n];
-   }
-main()
-{int ch,no,dir,next,i;
- char input,change,str[40];
- node *trans,*temp;//
- link *aj;//links 
- cout<<"Enter the no node : ";
- cin>>mxsize; 
- do{cout<<"\nMenu : ";
+int main(){
+	int ch,no,dir,next,i;
+ 	char input,change,str[40];
+ 	node *trans,*temp;//
+ 	link *aj;//links 
+ 	cout<<"Enter the no node : ";
+ 	cin>>mxsize; 
+ 	do{
+		cout<<"\nMenu : ";
     cout<<"\n1.Establish links : ";
     cout<<"\n2.Enter the string to check : ";
     cout<<"\n0.Exit:";
     cout<<"\nEnter your choice ";
     cin>>ch;
     switch(ch)
-   {case 1: cout<<"Enter the no of node : ";
+		{case 1: cout<<"Enter the no of node : ";
             cin>>no;
             temp=node_add(no);
             cout<<"Enter the input character : ";
